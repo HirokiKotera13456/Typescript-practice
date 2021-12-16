8cd7a20d745901902b0cbd187661ac8b85039057
nijimajohn <86758487+nijimajohn@users.noreply.github.com>
2021-12-16 12:07:46 +0900

    typescript-practice

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,8 @@
-export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p>{`${title}(ユーザー:${userid})`}</p>;
+import { TodoType } from "./types/todo";
+import { VFC } from "react";
+
+export const Todo: VFC<Omit<TodoType, "Id">> = (props) => {
+  const { title, userId, completed = false } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark}${title}(ユーザー:${userId})`}</p>;
 };
